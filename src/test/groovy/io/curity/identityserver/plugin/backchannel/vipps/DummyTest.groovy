df55aa57dee05f627316eb825fa981b69e82cbdf
Daniel Lindau <d.lindau@gmail.com>
2026-02-06 16:14:28 +0100

    test: Add dummy test so test task don't fail

---
+++ src/test/groovy/io/curity/identityserver/plugin/backchannel/vipps/DummyTest.groovy
@@ -0,0 +1,15 @@
+package io.curity.identityserver.plugin.backchannel.vipps
+
+import spock.lang.Specification
+
+/**
+ * Dummy test to ensure the test task doesn't fail when no unit tests exist.
+ * Unit tests can be added here as needed.
+ */
+class DummyTest extends Specification {
+
+    def "dummy test passes"() {
+        expect:
+        true
+    }
+}
